0a6a5f295ce531e2625daf2e591072dc4d5e368f
“matt.balcers” <matt@mattbalcers.com>
2016-02-21 20:44:50 +0000

    SkyBet Tech Test - Matt Balcers
    SkyBet Betting Slip
    
      git config --global user.email “matt@mattbalcers.com“
      git config --global user.name “matt.balcers”

---
+++ js/main.js
@@ -0,0 +1,164 @@
+//Bet Table Rendering
+
+function renderBets($container, $template, bets) {
+    $container.html('');
+    bets.forEach(function (bet) {
+        var $bet = $template.clone();
+        $bet.data(bet);
+        $bet.find('.bet__event').text(bet.event);
+        $bet.find('.bet__name').text(bet.name);
+        $bet.find('.bet__odds').text(formatOdds(bet.odds)).data('odds', bet.odds);
+        $container.append($bet);
+    });
+}
+
+function renderSelection($container, $selection) {
+    var id = 'selection-' + $selection.data('bet_id');
+    var sel = $selection.data();
+    $selection.attr('id', id);
+    $selection.find('.selection__title').text(
+        sel.event + ' - ' + sel.name + ' @ ' + formatOdds(sel.odds));
+    $selection.find('.selection__returns').text(' Potential Return £' +
+                                                formatReturns(sel.odds, 0));
+
+    $container.append($selection);
+}
+
+function renderReceipt($container, $receipt) {
+    var data = $receipt.data();
+    //this would be a nice use case for variable destructuring in ES6
+    var id = 'receipt-' + data.bet_id;
+    $receipt.attr('id', id);
+	
+    $receipt.find('.receipt__title').text(
+        data.event + ' - ' + data.name);
+    $receipt.find('.receipt__stake').text('£' +
+                                          data.stake +
+                                          ' @ ' +
+                                          formatOdds(data.odds) +
+                                          ' could return £' +
+                                          formatReturns(data.odds, data.stake));
+    $receipt.find('.receipt__transaction-id').text(data.transaction_id);
+    $container.append($receipt).toFixed(2);
+}
+
+function removeSelection(selectionId) {
+    $('#selection-' + selectionId).remove();
+}
+
+function formatOdds(odds) {
+    return odds.numerator === odds.denominator ? 'Evens' : odds.numerator + '/' + odds.denominator;
+}
+
+function formatReturns(odds, stake) {
+    var nil = 0;
+    return stake > 0 ? ((1 + (odds.numerator / odds.denominator)) * stake).toFixed(2) : nil.toFixed(2);
+}
+
+function betLoadError(error) {
+    console.error(error);
+}
+
+var templates = ["bet", "selection", "receipt"];
+var $templates = {};
+var betslip = [];
+var $bets = $('#bets'), $selections = $('#selections'), $receipts = $('#receipts');
+var $app = $('body');
+
+templates.forEach(function (template) {
+    $templates[template] = $($('#' + template).html());
+});
+
+$.get('http://skybettechtestapi.herokuapp.com/available')
+    .done(renderBets.bind(this, $bets, $templates['bet']))
+    .fail(betLoadError);
+
+/**
+ * @todo Think about how the betslip show behave when selections are added and removed
+ */
+$app.on('click', '.js-odds', function () {
+    var $bet = $(this).parent('.bet');
+    var betId = $bet.data('bet_id');
+
+    if (betslip.indexOf(betId) === -1) {
+        var $selection = $templates['selection'].clone();
+        $selection.data($bet.data());
+
+        renderSelection($selections, $selection);
+        betslip.push(betId);
+    } else {
+        removeSelection(betId);
+        betslip.splice(betslip.indexOf(betId), 1);
+    }
+});
+
+$app.on('submit', '.js-selection', function (e) {
+    e.preventDefault();
+    var $selection = $(this);
+
+    var stake = $selection.find('.js-stake').val();
+    var $button = $selection.find('.js-place-bet');
+
+    if (stake > 0) {
+        var data = $.extend($selection.data(), {stake: stake});
+
+        $button.attr('disabled', true);
+
+        $.ajax({
+            url: 'http://skybettechtestapi.herokuapp.com/bets',
+            method: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            dataType: 'json'
+        }).done(
+            function (data) {
+                //remove bet from selections and add to the receipts
+                removeSelection(data.bet_id);
+                var $receipt = $templates['receipt'].clone();
+                $receipt.data(data);
+                renderReceipt($receipts, $receipt);
+            }
+        ).fail(function (e) {
+                //@todo show error message returned from the server
+                $selection.addClass('is-invalid');
+                console.error(e);
+            }
+        );
+    } else {
+        $selection.addClass('is-invalid');
+    }
+});
+
+$app.on('keyup', '.js-stake', function () {
+    var $selection = $(this).closest('.js-selection');
+    var sel = $selection.data();
+
+    var stake = parseFloat($(this).val());
+    if (stake > 5) {
+        $selection.find('.js-place-bet').attr('disabled', false);
+    } else {
+        $selection.find('.js-place-bet').attr('disabled', true);
+    }
+    $selection.find('.js-returns').html('@ ' +
+                                        formatOdds(sel.odds) +
+                                        ' could return £' +
+                                        formatReturns(sel.odds, stake));
+});
+
+$app.on('click', '.js-toggle', function (e) {
+    e.preventDefault();
+    $(this).parent().find('.js-toggle-target').toggleClass('is-hidden');
+});
+
+$app.on('click', '.js-toggle-b', function (e) {
+    e.preventDefault();
+    $(this).parent().find('.js-toggle-target-b').toggleClass('is-hidden');
+});
+
+/*** if($(".js-toggle-target").is('is-hidden'))
+  {
+	$('i:active').show(); }
+	else
+{	$('i').show();
+	} ***/
+  
---
+++ submission-notes.md
@@ -0,0 +1,23 @@
+Spent around 3.5 hours looking at basic functionality and styling.
+
+Intended on creating multiple media queries, however given time constraints chose not to and initial design generated was to create pattern to be used on desktop as a columnal element which could be re-used or adapted across multiple platforms. Set at 320px max-width.
+
+I would of added the functionality to remove bets from a betslip, via the betslip, as this currently is missing - this is an area I identified for improvement in the JS provided.
+
+Adding the ability to switch between decimal and fractional odds.
+
+I'd of liked to have added some conditional/error state formatting to show different states on a betslip, should there be errors.
+
+Some additional visual styling for bet receipts as they currently appear quite dry.
+
+Also, some tooltips or info points to give users some introductory/basic information could of also been added given more time - further enhancing the experience (particulartly introductory experiences).
+
+By utilising some more of SkyBet's brand assets the styling of the entire design could be drastically improved.
+
+With the amount of bets pulled through on the demo API, this was not warranted, however I would of liked to look at adding search or some form of sortability to quickly find markets, enhancing the users journey. This is an area I identified for improvement in the JS provided - potentially for future candidates it would be good to see how designers/developers deal with scalability?
+
+------------------------------------------------------------------------------------------
+
+I know my CSS can be improved further to reduce inherited rules.
+
+I'd be more than happy to use some of the Tech Fund to continue to better and update my coding skills.
